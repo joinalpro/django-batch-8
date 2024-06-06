62c3c94be951101d42d5aa9455fd5edf384a5d70
Joinal Abden <joinalzx@gmail.com>
2024-06-06 11:23:17 +0600

    multivariable, string, slicing

---
+++ class-2/multiValue.py
@@ -0,0 +1,31 @@
+# Multiple Assignment Variable
+a = b = c = 33
+print(a)
+print(b)
+print(c)
+
+x, y, z = 1, 2, 3
+print(x)
+print(y)
+print(z)
+
+# slice start in 
+s = 'welcome'
+# print(s[3:])
+print(s[:-2])
+
+# convert in uppercase
+
+b = 'python uppercase'
+print(b.upper())
+c = 'lowercase'
+print(c.lower())
+
+# remove white space by using strip(), lstrip(), rstrip()
+
+d = '   Welcome'
+print(d.strip())
+
+# Replace String
+f = 'tea stall'
+print(f.replace('tea', 'cha'))
---
+++ class-2/string.py
@@ -0,0 +1,2 @@
+name = "miraz"
+print (type(name))
