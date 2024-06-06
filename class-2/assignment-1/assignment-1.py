cfdb946409ced1ccd44d4fc6cf092fe78d09a0f5
Joinal Abden <joinalzx@gmail.com>
2024-06-06 06:10:07 +0600

    python assignment for class 2

---
+++ class-2/assignment-1/assignment-1.py
@@ -0,0 +1,43 @@
+# Problem - 1
+print("AiQuest - Good Work")
+
+# Problem - 2
+batchNumber = int(input("Please inpurt your batch no "))
+print(batchNumber)
+
+# Problem - 3 Create 5 different valid Variables and print.
+first_name = "Joinal"
+last_name = "Abden"
+address = "Chandpur"
+print(address)
+country = "BD"
+print(country)
+postcode = 3611
+print(postcode)
+
+full_name = first_name +" "+ last_name
+print(full_name)
+
+# Problem - 4 Create Pascal Case Variable.
+VillageBeauty = "Green"
+print(VillageBeauty)
+# Problem - 5 Assign a single value to Multiple variables.
+a = b = c = "math"
+print(a)
+print(b)
+print(c)
+
+# Problem - 6 Input float value using input().
+num1 = float(input("enter a float number: "))
+print(num1)
+
+# Problem - 7 conver to upper case
+a = "Welcome to Django for web and Ai"
+make_upper = a.upper()
+print(make_upper)
+
+# Problem - 8  b = ‘ Python’remove whitespace.
+
+b = "   Python"
+remove_space = b.lstrip()
+print(remove_space)
