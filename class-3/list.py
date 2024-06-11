7a1041e252fd420f004613bb2b35a857bce1fb02
joinalzx <joinalzx@gmail.com>
2024-06-11 11:13:39 +0600

    list

---
+++ class-3/list.py
@@ -0,0 +1,40 @@
+course = ['python', 205, 'ML', 200, 'DL', 222,'Django', 266]
+# print(type(course))
+# print(len(course))
+
+# access item
+# print('Access Item: ', course[-3])
+
+# print(course.index('Django'))
+
+# print('Range: ', course[-6:-4])
+
+# list item
+course[1] = 22
+# print(course)
+
+# insert method
+# course.insert(2, 'Web Development')
+# print(course)
+
+# # append mehod
+# course.append("Teacher")
+# print(course)
+
+course.pop(1)
+# print(course)
+
+del course[2]
+# print(course)
+
+# Sorting
+
+alphabetic = ['zoom', 'voov','doob', 'khoob','aoob']
+# alphabetic.sort()
+# print(alphabetic)
+
+# numeric
+numberic = [ 56, 23, 67, 23, 89,232, 22]
+numberic.sort()
+# print(numberic.sort())
+print(numberic)
