e1fb78e2fa3edb65b406a3f7057e310874787a87
joinalzx <joinalzx@gmail.com>
2024-06-13 17:27:23 +0600

    function

--- advance-python-self-study/function-argument.py
+++ advance-python-self-study/function-argument.py
@@ -13,7 +13,19 @@
 # print(student_marks(4))
 
 # star(*) argument / *args
-def divide_by (*args):
-    for num in args:
-        print(f"{num * 2} showing multple result")
-divide_by(2, 3, 4, 6)
+# def divide_by (*args):
+#     for num in args:
+#         print(f"{num * 2} showing multple result")
+# divide_by(2, 3, 4, 6)
+
+# try:
+#     f= open('file.py', 'x')
+# except:
+#     print('do not show the error')
+    
+# write_name = open('file.py','w')
+# write_name.write('Hello Python developer!')
+# write_name.close()
+
+with open("file.py","r+") as f:
+    print(f.read())
